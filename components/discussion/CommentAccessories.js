f9b303e88f8da2c2967b01348ef76090b17ab9e0
ultddeveloper <andrew.mutien@gmail.com>
2022-02-21 00:43:29 +0800

    Implement Comment Field with commenting function

--- components/discussion/CommentAccessories.js
+++ components/discussion/CommentAccessories.js
@@ -1,29 +1,131 @@
+import { useState, useRef } from "react";
+import { PaperAirplaneIcon, XIcon } from "@heroicons/react/outline";
+import { postCommentBE } from "../../lib/discussion/comments";
+
+const NewComment = ({ boardId, addComment }) => {
+    const commentDiv = useRef(null);
+    const [enteringComment, setEnteringComment] = useState(false);
+    const [commentSide, setCommentSide] = useState(null);
+
+    return (
+        <div className="flex w-full items-center justify-end">
+            <div
+                className={`border-primary-700 transition-width relative mt-2 mb-4 flex flex-grow-0 items-center border px-3 ${
+                    enteringComment ? "w-full rounded-2xl" : "h-9 w-9 rounded-full"
+                } `}
+            >
+                {enteringComment && (
+                    <div
+                        ref={commentDiv}
+                        className={`my-auto h-full w-full bg-transparent pl-2 pr-32 text-lg leading-9 focus:outline-none`}
+                        contentEditable={true}
+                        onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                                e.preventDefault();
+                            }
+                        }}
+                    />
+                )}
+                {enteringComment ? (
+                    <div className="absolute top-1.5 right-2 flex items-center justify-end gap-2">
+                        <button
+                            className={`border-primary-700 ${
+                                commentSide === "sup"
+                                    ? " text-primary-50 bg-primary-700"
+                                    : "text-primary-700  bg-neutral-100"
+                            } h-6 w-6 flex-shrink-0 rounded-full border text-center text-sm `}
+                            onClick={() => {
+                                if (commentSide === "sup") setCommentSide(null);
+                                else setCommentSide("sup");
+                            }}
+                        >
+                            支
+                        </button>
+                        <button
+                            className={`border-primary-700 ${
+                                commentSide === "agn"
+                                    ? " text-primary-50 bg-primary-700"
+                                    : "text-primary-700  bg-neutral-100"
+                            } h-6 w-6 flex-shrink-0 rounded-full border text-center text-sm `}
+                            onClick={() => {
+                                if (commentSide === "agn") setCommentSide(null);
+                                else setCommentSide("agn");
+                            }}
+                        >
+                            反
+                        </button>
+                        <button
+                            className={`text-primary-700 h-6 w-6 flex-shrink-0`}
+                            onClick={async () => {
+                                if (commentDiv.current.innerText !== "") {
+                                    addComment(
+                                        await postCommentBE(
+                                            commentDiv.current.innerText,
+                                            boardId,
+                                            commentSide
+                                        )
+                                    );
+                                    setEnteringComment(false);
+                                }
+                            }}
+                        >
+                            <PaperAirplaneIcon className="h-6 w-6" />
+                        </button>
+                        <button
+                            className={`text-primary-700 h-6 w-6 flex-shrink-0 `}
+                            onClick={() => {
+                                setEnteringComment(!enteringComment);
+                            }}
+                        >
+                            <XIcon className="h-6 w-6" />
+                        </button>
+                    </div>
+                ) : (
+                    <button
+                        className={`absolute top-1.5 right-1 m-auto h-6 w-6 flex-shrink-0 `}
+                        onClick={() => {
+                            setEnteringComment(!enteringComment);
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="fill-primary-700 h-6 w-6"
+                        >
+                            <path d="M4 14a1 1 0 0 1 .3-.7l11-11a1 1 0 0 1 1.4 0l3 3a1 1 0 0 1 0 1.4l-11 11a1 1 0 0 1-.7.3H5a1 1 0 0 1-1-1v-3z" />
+                            <rect width="20" height="2" x="2" y="20" rx="1" />
+                        </svg>
+                    </button>
+                )}
+            </div>
+        </div>
+    );
+};
+
 const NoCommentsDisplay = () => {
     return (
-        <div className=" w-11/12 h-40 mx-auto my-2 flex" >
-            <div className='m-auto'>
-                <h1 className='text-xl text-center'>
+        <div className=" mx-auto my-2 flex h-40 w-11/12">
+            <div className="m-auto">
+                <h1 className="text-center text-xl">
                     目前還沒有留言呢 <br />
                     成為第一個留言的人吧
                 </h1>
             </div>
         </div>
-    )
-}
+    );
+};
 
 const LoadingSkeleton = () => {
     return (
-        <div className="w-full flex gap-3 py-2">
-            <img className='w-7 h-7 flex-shrink-0 rounded-full animate-pulse bg-gray-300 p-1 border-2 border-gray-300' />
-            <div className='flex-grow'>
-                <div className="w-36 h-5 bg-gray-300 animate-pulse rounded-lg mb-2"></div>
-                <div className="h-5 bg-gray-300 animate-pulse rounded-lg my-1"></div>
-                <div className="h-5 bg-gray-300 animate-pulse rounded-lg mt-1 mb-3"></div>
-                <div className="w-32 h-5 bg-gray-300 animate-pulse rounded-lg my-1"></div>
-
+        <div className="flex w-full gap-3 py-2">
+            <img className="h-7 w-7 flex-shrink-0 animate-pulse rounded-full border-2 border-gray-300 bg-gray-300 p-1" />
+            <div className="flex-grow">
+                <div className="mb-2 h-5 w-36 animate-pulse rounded-lg bg-gray-300"></div>
+                <div className="my-1 h-5 animate-pulse rounded-lg bg-gray-300"></div>
+                <div className="mt-1 mb-3 h-5 animate-pulse rounded-lg bg-gray-300"></div>
+                <div className="my-1 h-5 w-32 animate-pulse rounded-lg bg-gray-300"></div>
             </div>
-        </div >
-    )
-}
+        </div>
+    );
+};
 
-export { NoCommentsDisplay, LoadingSkeleton }
+export { NewComment, NoCommentsDisplay, LoadingSkeleton };
--- components/discussion/CommentResponseField.js
+++ components/discussion/CommentResponseField.js
@@ -1,34 +1,43 @@
-import { ReplyIcon } from '@heroicons/react/outline'
-import { cloneDeep } from 'lodash'
-import CommentCard from './CommentCard'
+import { ReplyIcon } from "@heroicons/react/outline";
+import CommentCard from "./CommentCard";
 
-const CommentResponseField = ({ boardId, onSide, commentId, commentData, delComment, fetchMoreReplies = null }) => {
-
+const CommentResponseField = ({
+    boardId,
+    onSide,
+    commentId,
+    commentData,
+    delComment,
+    fetchMoreReplies = null,
+}) => {
     const ShowRepliesButton = () => {
         return (
-            <button className='text-neutral-500 flex items-start gap-1' onClick={fetchMoreReplies}>
-                <ReplyIcon className='inline rotate-180 w-5 h-5' />
-                <p className='inline text-sm'>查看更多回覆</p>
+            <button className="flex items-start gap-1 text-neutral-500" onClick={fetchMoreReplies}>
+                <ReplyIcon className="inline h-5 w-5 rotate-180" />
+                <p className="inline text-sm">查看更多回覆</p>
             </button>
-        )
-    }
+        );
+    };
 
     return (
-        <div className="w-[88%] md:w-11/12 lg:w-[88%] xl:w-11/12 bg-neutral-50 mt-2 ml-auto flex flex-col">
+        <div className="mt-2 ml-auto flex w-[88%] flex-col bg-neutral-50 md:w-11/12 lg:w-[88%] xl:w-11/12">
             {commentData.map((cmt, i) => {
-                return <CommentCard
-                    boardId={boardId}
-                    onSide={onSide}
-                    key={"r" + i}
-                    cmtdata={cmt}
-                    motherComment={commentId}
-                    delComment={() => { delComment(i) }}
-                    replyFunction={null}
-                />
+                return (
+                    <CommentCard
+                        boardId={boardId}
+                        onSide={onSide}
+                        key={"r" + i}
+                        cmtdata={cmt}
+                        motherComment={commentId}
+                        delComment={() => {
+                            delComment(i);
+                        }}
+                        replyFunction={null}
+                    />
+                );
             })}
             {fetchMoreReplies !== null && <ShowRepliesButton />}
-        </div >
-    )
-}
+        </div>
+    );
+};
 
-export default CommentResponseField
+export default CommentResponseField;
