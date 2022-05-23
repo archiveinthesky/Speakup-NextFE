525191e1e4369d261dd07ee847215046772e430b
ultddeveloper <andrew.mutien@gmail.com>
2022-05-23 14:41:59 +0800

    Add admin homepage

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,13 +1,45 @@
 import Header from '../../components/navbar/Header';
-import Sidebar from '../../components/navbar/Sidebar.js';
+import AdminSidebar from '../../components/navbar/AdminSidebar';
+import {
+    AnnotationIcon,
+    ClipboardCheckIcon,
+    PencilAltIcon,
+} from '@heroicons/react/outline';
+import Link from 'next/link';
 
 const AdminHomePage = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-screen bg-neutral-100">
             <Header />
-            <Sidebar />
-            <div className="mt-16 ml-64 pt-16 pl-20">
-                <h1 className="text-4xl text-primary-800"></h1>
+            <AdminSidebar />
+            <div className="mt-16 ml-64 flex h-[calc(100vh-156px)] flex-col items-center justify-center gap-8 pt-16 pl-20 text-primary-800">
+                <h1 className="text-4xl">歡迎回到Speakup創作者管理介面</h1>
+                <div className="flex gap-8">
+                    <Link href="/admin/manage/content">
+                        <button className="block rounded-lg border-4 border-primary-800 px-12 py-16 transition-colors hover:bg-white/20">
+                            <PencilAltIcon className="h-24 w-24" />
+                            <h2 className="mt-2 text-center text-2xl">
+                                議題總覽
+                            </h2>
+                        </button>
+                    </Link>
+                    <Link href="/admin/moderate/content">
+                        <button className="block rounded-lg border-4 border-primary-800 px-12 py-16 transition-colors hover:bg-white/20">
+                            <ClipboardCheckIcon className="h-24 w-24" />
+                            <h2 className="mt-2 text-center text-2xl">
+                                待審議題
+                            </h2>
+                        </button>
+                    </Link>
+                    <Link href="/admin/moderate/comments">
+                        <button className="block rounded-lg border-4 border-primary-800 px-12 py-16 transition-colors hover:bg-white/20">
+                            <AnnotationIcon className="h-24 w-24" />
+                            <h2 className="mt-2 text-center text-2xl">
+                                留言管理
+                            </h2>
+                        </button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
