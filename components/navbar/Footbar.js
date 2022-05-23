88db7bf2dcb3d1570756621a16ad783d745a08a2
ultddeveloper <andrew.mutien@gmail.com>
2022-05-23 11:55:49 +0800

    Add sidebar tags & footbar page

--- components/navbar/Footbar.js
+++ components/navbar/Footbar.js
@@ -8,32 +8,75 @@ import {
     UserCircleIcon,
 } from '@heroicons/react/outline';
 
+import {
+    HomeIcon as HomeIconSolid,
+    SearchIcon as SearchIconSolid,
+    BookmarkIcon as BookmarkIconSolid,
+    BellIcon as BellIconSolid,
+    UserCircleIcon as UserCircleIconSolid,
+} from '@heroicons/react/solid';
+import { useRouter } from 'next/router';
+
 const Footbar = () => {
+    const router = useRouter();
+
+    const pageUrl = (() => {
+        let url = '',
+            len = router.pathname.length;
+        for (let i = 0; i < len; i++) {
+            let currentChar = router.pathname[len - i - 1];
+            if (i == 0 && currentChar == '/') {
+            } else if (currentChar == '/') break;
+            url += currentChar;
+        }
+        return url.split('').reverse().join('');
+    })();
+
     return (
         <div className="fixed left-0 bottom-0 z-20 flex h-16 w-full items-center justify-around border-t border-gray-400 bg-neutral-50 px-5 text-primary-900 lg:hidden">
             <Link href="/home">
                 <a>
-                    <HomeIcon className="h-8 w-8" />
+                    {pageUrl == 'home' ? (
+                        <HomeIconSolid className="h-8 w-8" />
+                    ) : (
+                        <HomeIcon className="h-8 w-8" />
+                    )}
                 </a>
             </Link>
             <Link href="/search">
                 <a>
-                    <SearchIcon className="h-8 w-8" />
+                    {pageUrl == 'search' || pageUrl == 'results' ? (
+                        <SearchIconSolid className="h-8 w-8" />
+                    ) : (
+                        <SearchIcon className="h-8 w-8" />
+                    )}
                 </a>
             </Link>
             <Link href="/collections">
                 <a>
-                    <BookmarkIcon className="h-8 w-8" />
+                    {pageUrl == 'collections' ? (
+                        <BookmarkIconSolid className="h-8 w-8" />
+                    ) : (
+                        <BookmarkIcon className="h-8 w-8" />
+                    )}
                 </a>
             </Link>
             <Link href="/notifications">
                 <a>
-                    <BellIcon className="h-8 w-8" />
+                    {pageUrl == 'notifications' ? (
+                        <BellIconSolid className="h-8 w-8" />
+                    ) : (
+                        <BellIcon className="h-8 w-8" />
+                    )}
                 </a>
             </Link>
-            <Link href="/settings">
+            <Link href="/aboutuser">
                 <a>
-                    <UserCircleIcon className="h-8 w-8" />
+                    {pageUrl == 'aboutuser' ? (
+                        <UserCircleIconSolid className="h-8 w-8" />
+                    ) : (
+                        <UserCircleIcon className="h-8 w-8" />
+                    )}
                 </a>
             </Link>
         </div>
--- components/navbar/Sidebar.js
+++ components/navbar/Sidebar.js
@@ -8,19 +8,39 @@ import {
 } from '@heroicons/react/outline';
 
 const Sidebar = ({ retractable }) => {
-    const tags = ['教育', '娛樂', '媒體', '科技'];
-    //['娛樂', '環境', '司法', '國家發展', '經濟', '少數族群', '媒體', '醫藥', '道德', '政治', '教育', '家庭', '女性', '自由', '宗教', '科技', '社會政策', '社會運動', '體育'];
+    const tags =
+        //['教育', '娛樂', '媒體', '科技'];
+        [
+            '娛樂',
+            '環境',
+            '司法',
+            '國家發展',
+            '經濟',
+            '少數族群',
+            '媒體',
+            '醫藥',
+            '道德',
+            '政治',
+            '教育',
+            '家庭',
+            '女性',
+            '自由',
+            '宗教',
+            '科技',
+            '社會政策',
+            '社會運動',
+            '體育',
+        ];
 
     return (
         <div
-            className={`group fixed top-14 left-0 hidden h-[calc(100vh-56px)] flex-shrink-0 flex-col rounded-r-[32px] bg-neutral-50 transition-width duration-500 ease-out overflow-x-hidden lg:flex ${
+            className={`group fixed top-14 left-0 hidden h-[calc(100vh-56px)] flex-shrink-0 flex-col overflow-hidden overflow-x-hidden rounded-r-[32px] bg-neutral-50 pt-6 transition-width duration-500 ease-out lg:flex ${
                 retractable
                     ? 'w-20 hover:w-64 hover:drop-shadow-xl lg:flex'
                     : 'w-64'
             } `}
         >
-            <div className="h-6" />
-            <div className="w-full">
+            <div className="h-44 w-full">
                 <ul className="mx-auto pl-7 text-primary-900">
                     <Link href="/home">
                         <li className="flex cursor-pointer list-none gap-4 py-3">
@@ -66,15 +86,15 @@ const Sidebar = ({ retractable }) => {
                     </Link>
                 </ul>
             </div>
+            <div className="py-3">
+                <hr className="mx-auto w-5/6 border-t-2 border-gray-300" />
+            </div>
             <div
-                className={`${
+                className={`flex-grow overflow-y-auto pb-6 ${
                     retractable ? 'hidden group-hover:block' : 'block'
                 }`}
             >
-                <div className="py-3">
-                    <hr className="mx-auto w-5/6 border-t-2 border-gray-300" />
-                </div>
-                <ul className="h-full list-none overflow-y-auto pl-20 overflow-x-hidden">
+                <ul className="h-full list-none overflow-x-hidden pl-20">
                     {tags.map((tag, i) => {
                         return (
                             <Link
