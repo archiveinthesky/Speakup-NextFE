69d586f8d25c089c8dd8b0bb59dcede58ba52bce
ultddeveloper <andrew.mutien@gmail.com>
2022-05-28 00:21:55 +0800

    Comment unused code in settings

--- pages/user/settings.js
+++ pages/user/settings.js
@@ -17,47 +17,47 @@ const Settings = () => {
     const [initValues, setInitValues] = useState({});
     const { data: session } = useSession();
 
-    const { data, error, isLoading, refetch } = useQuery(
-        'settings',
-        async () => {
-            let response = await fetch(
-                `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/user/data`,
-                {
-                    method: 'GET',
-                    headers: {
-                        Authorization: `Token ${session.authToken}`,
-                        Accept: 'application/json',
-                    },
-                }
-            );
-            if (!response.ok) throw new Error('Fetch failed');
-            return response.json();
-        },
-        { refetchOnWindowFocus: false, enabled: false }
-    );
+    // const { data, error, isLoading, refetch } = useQuery(
+    //     'settings',
+    //     async () => {
+    //         let response = await fetch(
+    //             `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/user/data`,
+    //             {
+    //                 method: 'GET',
+    //                 headers: {
+    //                     Authorization: `Token ${session.authToken}`,
+    //                     Accept: 'application/json',
+    //                 },
+    //             }
+    //         );
+    //         if (!response.ok) throw new Error('Fetch failed');
+    //         return response.json();
+    //     },
+    //     { refetchOnWindowFocus: false, enabled: false }
+    // );
 
-    useEffect(() => {
-        if (session) refetch();
-    }, [session]);
+    // useEffect(() => {
+    //     if (session) refetch();
+    // }, [session]);
 
-    useEffect(() => {
-        if (data) {
-            let newData = cloneDeep(data);
-            newData.birthday = dayjs(data.birthday).toDate();
-            setInitValues(newData);
-        }
-    }, [data]);
+    // useEffect(() => {
+    //     if (data) {
+    //         let newData = cloneDeep(data);
+    //         newData.birthday = dayjs(data.birthday).toDate();
+    //         setInitValues(newData);
+    //     }
+    // }, [data]);
 
-    useEffect(() => {
-        if (error)
-            showNotification({
-                title: '資料獲取失敗',
-                message: '請重新整理頁面',
-                color: 'red',
-                disallowClose: true,
-                autoClose: false,
-            });
-    }, [error]);
+    // useEffect(() => {
+    //     if (error)
+    //         showNotification({
+    //             title: '資料獲取失敗',
+    //             message: '請重新整理頁面',
+    //             color: 'red',
+    //             disallowClose: true,
+    //             autoClose: false,
+    //         });
+    // }, [error]);
 
     return (
         <div className="fixed top-0 left-0 h-screen w-screen overflow-y-scroll bg-neutral-100">
@@ -67,14 +67,14 @@ const Settings = () => {
             <div className="w-full px-6 md:px-10 lg:py-6 lg:pl-72">
                 <div className="mx-auto flex max-w-7xl flex-grow-0 flex-col gap-10 pt-20 md:pt-24">
                     <div className="relative">
-                        <LoadingOverlay visible={isLoading} zIndex={10} />
-                        {error && <Overlay opacity={0.6} zIndex={10} />}
+                        {/* <LoadingOverlay visible={isLoading} zIndex={10} /> */}
+                        {/* {error && <Overlay opacity={0.6} zIndex={10} />} */}
                         <BasicSettingsCard initValues={initValues} />
                     </div>
 
                     <div className="relative">
-                        <LoadingOverlay visible={isLoading} zIndex={10} />
-                        {error && <Overlay opacity={0.6} zIndex={10} />}
+                        {/* <LoadingOverlay visible={isLoading} zIndex={10} /> */}
+                        {/* {error && <Overlay opacity={0.6} zIndex={10} />} */}
                         <AccountActions />
                     </div>
                 </div>
